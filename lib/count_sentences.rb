0bcd669806ab7d4e802d92de2b600754d5cb7637
david-commit <david.ondiege@students.moringaschool.com>
2022-10-20 22:56:18 +0300

    COmpleted code-along

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
-
+  
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sentences = 0
+    
+    punctuation = [".", "?", "!"]
+    # iterate over every character in the string
+    self.chars.each.with_index do |char, index|
 
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
   end
+
 end
